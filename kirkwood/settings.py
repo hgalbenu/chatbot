ab238fbd0eae9212b27d4f2dc4a70dfdb416b435
horiagalbenu <horia.galbenu@rodeapps.com>
2017-04-19 15:24:27 +0300

    hopefully fixed heroku stuff

--- kirkwood/settings.py
+++ kirkwood/settings.py
@@ -153,10 +153,7 @@ if ENVIRONMENT == 'STAGING':
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
     RAVEN_CONFIG = {
-        'dsn': os.getenv('RAVEN_DSN'),
-        # If you are using git, you can also automatically configure the
-        # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+        'dsn': os.getenv('RAVEN_DSN')
     }
 
 # Motion AI
